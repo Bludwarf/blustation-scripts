2e928a4a109298c547742ede46e8da8617779ed1
Bludwarf <bludwarf@gmail.com>
2026-05-09 06:17:59 +0200

    feat: mv-freebox

---
+++ mv-freebox.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mv --update /volume1/Freebox/BLUSB2026/Enregistrements/* /volume1/video/Enregistrements/
+mv --update /volume1/Freebox/BLUSB2026/.fbxprivrec/* /volume1/video/Enregistrements/_fbxprivrec/
